0a05023f48eed3d2858ce718b96ea32292d41eae
rayakhader <rayakhader20@gmail.com>
2025-04-07 20:18:42 +0200

    edit renderPagination functionality for no tasks found

--- script.js
+++ script.js
@@ -146,8 +146,10 @@ document.addEventListener('DOMContentLoaded', async () => {
     list.innerHTML = '';
     const totalPages = Math.ceil(JSON.parse(localStorage.getItem('tasks')).length / itemsPerPage);
     if(totalPages === 0){
+      document.querySelector('#pagination').style.display = 'none';
       return;
     }
+    document.querySelector('#pagination').style.display = 'flex';
     for (let i = 1; i <= totalPages; i++) {
       const li = document.createElement('li');
       li.innerHTML = `<a href="#" onclick="displayPage(${i})">${i}</a>`;
